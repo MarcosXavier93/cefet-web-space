9cb8098efce5c5a9341c07360bd3af8b3a20a097
Marcos Xavier <marcos_mjsx@hotmail.com>
2021-06-21 19:20:30 -0300

    Finalizando exercicio 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let botoes = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let botao of botoes) {
+    botao.addEventListener("click", () => {
+    botao.parentNode.classList.toggle("expandido");
+      if (botao.innerHTML == "+") {
+        botao.innerHTML = "-"
+      }
+      else {
+        botao.innerHTMl = "+"
+       }
+  });
+}
